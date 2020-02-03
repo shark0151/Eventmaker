e27e414510a4cf5d007dcc67027832493e8f4a4b
Vlad Alexandru Faget <vlad.faget@outlook.com>
2020-02-03 11:23:57 +0100

    SubAssigment 1 Complete

---
+++ Model/Event.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventmaker.Model
+{
+    class Event
+    {
+        private int Id;
+        private string Name;
+        private string Description;
+        private string Place;
+        private DateTime DateTime;
+
+        public Event(int id,string name,string desc,string place,DateTime dateTime)
+        {
+            Id = id;
+            Name = name;
+            Place = place;
+            Description = desc;
+            DateTime = dateTime;
+        }
+
+        override public string  ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Model/EventCatalogSingleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventmaker.Model
+{
+    class EventCatalogSingleton
+    {
+        private static EventCatalogSingleton _instance;
+        private ObservableCollection<Event> Catalog = new ObservableCollection<Event>();
+        private EventCatalogSingleton()
+        {
+            Add(new Event(1, "Initial", "test", "Singleton", DateTime.Now));
+        }
+
+        public static EventCatalogSingleton Instance()
+        {
+            if(_instance == null)
+            {
+                _instance = new EventCatalogSingleton();
+            }
+            return _instance;
+        }
+
+        public void Add(Event newEvent)
+        {
+            Catalog.Add(newEvent);
+        }
+    }
+}
